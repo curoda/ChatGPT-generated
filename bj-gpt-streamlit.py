1f0339cf3e29c6dd0d768cb779dd816cbceb3130
Curt Odar <curoda@users.noreply.github.com>
2023-02-28 13:51:44 -0500

    Update bj-gpt-streamlit.py
    updated the basic_strategy function to match the logic found here: https://www.blackjackapprenticeship.com/blackjack-strategy-charts/

--- bj-gpt-streamlit.py
+++ bj-gpt-streamlit.py
@@ -34,47 +34,132 @@ def dealer_turn(dealer_hand):
         dealer_hand.append(deck.pop())
 
 # Define a function to simulate the player's decision using basic strategy
-def basic_strategy(player_hand, dealer_card, num_splits, can_double, can_surrender):
-    player_total = calculate_hand(player_hand)
-    if num_splits > 0 and player_hand[0] == player_hand[1]:
-        if player_hand[0] in [2, 3] and dealer_card in [2, 3, 4, 5, 6, 7]:
-            return 'split'
-        elif player_hand[0] in [4, 5] and dealer_card in [2, 3, 4, 5, 6]:
-            return 'split'
-        elif player_hand[0] == 6 and dealer_card in [2, 3, 4, 5, 6]:
-            return 'split'
-        elif player_hand[0] == 7 and dealer_card in [2, 3, 4, 5, 6]:
-            return 'split' if num_splits < 3 else 'hit'
-        elif player_hand[0] == 8 and dealer_card in [2, 3, 4, 5, 6]:
-            return 'split'
-        elif player_hand[0] == 9 and dealer_card in [2, 3, 4, 5, 6, 8, 9]:
-            return 'split' if num_splits < 3 else 'stand'
-        elif player_hand[0] == 10 and dealer_card in [2, 3, 4, 5, 6, 7, 8, 9]:
-            return 'split'
-        elif player_hand[0] == 'A' and dealer_card in [2, 3, 4, 5, 6, 8, 9, 10, 'A']:
-            return 'split' if num_splits < 3 else 'stand'
-    if can_double and player_total in [9, 10, 11]:
-        if player_total == 9 and dealer_card in [2, 3, 4, 5, 6, 8, 9]:
-            return 'double'
-        elif player_total == 10 and dealer_card in [2, 3, 4, 5, 6, 7, 8, 9]:
-            return 'double'
-        elif player_total == 11 and dealer_card in [2, 3, 4, 5, 6, 7, 8, 9, 10, 'A']:
-            return 'double'
-    if can_surrender and player_total in [15, 16]:
-        if player_total == 15 and dealer_card in [9, 10, 'A']:
-            return 'surrender'
-        elif player_total == 16 and dealer_card in [9, 10, 'A', 7]:
-            return 'surrender'
-    if player_total >= 17:
-        return 'stand'
-    elif player_total <= 11:
-        return 'hit'
-    elif player_total == 12 and dealer_card in [2, 3, 7, 8, 9, 10, 'A']:
-        return 'hit'
-    elif player_total in [13, 14, 15, 16] and dealer_card in [2, 3, 4, 5, 6]:
-        return 'stand'
+def basic_strategy(player_hand, dealer_card):
+    """
+    Implements a perfect blackjack strategy based on the player's hand and the dealer's up card.
+    Args:
+    - player_hand: a list of strings representing the player's hand, e.g. ["A", "5"]
+    - dealer_card: a string representing the dealer's up card, e.g. "10"
+    Returns:
+    - A string representing the recommended action, one of "stand", "hit", "split", "double", or "surrender"
+    """
+
+    # Helper functions to check if the player's hand is a soft hand or a pair
+    def is_soft_hand(hand):
+        return any(card == "A" for card in hand) and sum(get_card_value(card) for card in hand) <= 11
+
+    def is_pair(hand):
+        return len(hand) == 2 and hand[0] == hand[1]
+
+    # Helper function to get the value of a card
+    def get_card_value(card):
+        if card in ["J", "Q", "K"]:
+            return 10
+        elif card == "A":
+            return 11
+        else:
+            return int(card)
+
+    # Helper function to get the value of a hand
+    def get_hand_value(hand):
+        value = sum(get_card_value(card) for card in hand)
+        if value > 21 and "A" in hand:
+            value -= 10
+        return value
+
+    # Helper function to check if a hand can be doubled
+    def can_double(hand):
+        return len(hand) == 2
+
+    # Helper function to check if a hand can be split
+    def can_split(hand):
+        return len(hand) == 2 and hand[0] == hand[1]
+
+    # Implement the rules for surrender
+    if len(player_hand) == 2 and is_soft_hand(player_hand):
+        if get_hand_value(player_hand) == 15 and dealer_card in ["10", "J", "Q", "K"]:
+            return "surrender"
+        elif get_hand_value(player_hand) == 16 and dealer_card in ["9", "10", "J", "Q", "K", "A"]:
+            return "surrender"
+
+    # Implement the rules for pairs
+    if can_split(player_hand):
+        if player_hand[0] == "A":
+            return "split"
+        elif player_hand[0] == "8":
+            return "split"
+        elif player_hand[0] == "7" and dealer_card in ["2", "3", "4", "5", "6", "7"]:
+            return "split"
+        elif player_hand[0] == "6" and dealer_card in ["2", "3", "4", "5", "6"]:
+            return "split"
+        elif player_hand[0] == "5" and dealer_card in ["2", "3", "4", "5", "6", "7", "8", "9"]:
+            return "double"
+        elif player_hand[0] == "4" and dealer_card in ["5", "6"]:
+            return "split"
+        elif player_hand[0] == "3" and dealer_card in ["2", "3", "4", "5", "6", "7"]:
+            return "split"
+        elif player_hand[0] == "2" and dealer_card in ["2", "3", "4", "5", "6", "7"]:
+            return "split"
+        elif player_hand[0] == "9" and dealer_card in ["2", "3", "4", "5", "6", "8", "9"]:
+            return "stand"
+          
+    # Implement the rules for soft hands
+    if is_soft_hand(player_hand):
+        if get_hand_value(player_hand) == 20:
+            return "stand"
+        elif get_hand_value(player_hand) == 19 and dealer_card == "6":
+            return "double"
+        elif get_hand_value(player_hand) == 19 and dealer_card != "6":
+            return "stand"
+        elif get_hand_value(player_hand) == 18 and dealer_card in ["2", "3", "4", "5", "6"]:
+            return "double"
+        elif get_hand_value(player_hand) == 18 and dealer_card in ["9", "10", "J", "Q", "K", "A"]:
+            return "hit"
+        elif get_hand_value(player_hand) == 18 and dealer_card in ["7", "8"]:
+            return "stand"
+        elif get_hand_value(player_hand) == 17 and dealer_card in ["3", "4", "5", "6"]:
+            return "double"
+        elif get_hand_value(player_hand) == 16 and dealer_card in ["4", "5", "6"]:
+            return "double"
+        elif get_hand_value(player_hand) == 15 and dealer_card in ["4", "5", "6"]:
+            return "double"
+        elif get_hand_value(player_hand) == 14 and dealer_card in ["5", "6"]:
+            return "double"
+        elif get_hand_value(player_hand) == 13 and dealer_card in ["5", "6"]:
+            return "double"
+        else:
+            return "hit"
+
+    # Implement the rules for hard hands
+    if get_hand_value(player_hand) >= 17:
+        return "stand"
+    elif get_hand_value(player_hand) == 16 and dealer_card in ["2", "3", "4", "5", "6"]:
+        return "stand"
+    elif get_hand_value(player_hand) == 15 and dealer_card in ["2", "3", "4", "5", "6"]:
+        return "stand"
+    elif get_hand_value(player_hand) == 14 and dealer_card in ["2", "3", "4", "5", "6"]:
+        return "stand"
+    elif get_hand_value(player_hand) == 13 and dealer_card in ["2", "3", "4", "5", "6"]:
+        return "stand"
+    elif get_hand_value(player_hand) == 12 and dealer_card in ["4", "5", "6"]:
+        return "stand"
+    elif get_hand_value(player_hand) == 11:
+        if can_double(player_hand):
+            return "double"
+        else:
+            return "hit"
+    elif get_hand_value(player_hand) == 10:
+        if can_double(player_hand) and dealer_card in ["2", "3", "4", "5", "6", "7", "8", "9"]:
+            return "double"
+        else:
+            return "hit"
+    elif get_hand_value(player_hand) == 9:
+        if can_double(player_hand) and dealer_card in ["3", "4", "5", "6"]:
+            return "double"
+        else:
+            return "hit"
     else:
-        return 'hit'
+        return "hit"
 
 
 # Define a function to play a game of blackjack
@@ -94,7 +179,7 @@ def play_blackjack(num_decks, num_hands, can_double, can_surrender, bet, stop):
         player_hand = [deck.pop(), deck.pop()]
         can_split = num_splits < 3 and player_hand[0] == player_hand[1]
         player_busted = False
-        while basic_strategy(player_hand, dealer_hand[0], num_splits, can_double, can_surrender) == 'hit':
+        while basic_strategy(player_hand, dealer_hand[0]) == 'hit':
             player_hand.append(deck.pop())
             if calculate_hand(player_hand) > 21:
                 player_busted = True
@@ -109,18 +194,18 @@ def play_blackjack(num_decks, num_hands, can_double, can_surrender, bet, stop):
                 player_hand_2 = [player_hand[1], deck.pop()]
                 can_split_1 = num_splits < 3 and player_hand_1[0] == player_hand_1[1]
                 can_split_2 = num_splits < 3 and player_hand_2[0] == player_hand_2[1]
-                if basic_strategy(player_hand_1, dealer_hand[0], num_splits, can_double, can_surrender) == 'stand':
+                if basic_strategy(player_hand_1, dealer_hand[0]) == 'stand':
                     dealer_turn(dealer_hand)
                 else:
-                    while basic_strategy(player_hand_1, dealer_hand[0], num_splits, can_double, can_surrender) == 'hit':
+                    while basic_strategy(player_hand_1, dealer_hand[0]) == 'hit':
                         player_hand_1.append(deck.pop())
                         if calculate_hand(player_hand_1) > 21:
                             break
                     dealer_turn(dealer_hand)
-                if basic_strategy(player_hand_2, dealer_hand[0], num_splits, can_double, can_surrender) == 'stand':
+                if basic_strategy(player_hand_2, dealer_hand[0]) == 'stand':
                     dealer_turn(dealer_hand)
                 else:
-                    while basic_strategy(player_hand_2, dealer_hand[0], num_splits, can_double, can_surrender) == 'hit':
+                    while basic_strategy(player_hand_2, dealer_hand[0]) == 'hit':
                         player_hand_2.append(deck.pop())
                         if calculate_hand(player_hand_2) > 21:
                             break
